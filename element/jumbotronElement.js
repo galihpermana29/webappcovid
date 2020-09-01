4ca045c2152e5e852b1297dd2ad42400721c16de
Galih Permana <54941568+galihpermana29@users.noreply.github.com>
2020-09-01 09:44:53 +0700

    Update jumbotronElement.js

--- element/jumbotronElement.js
+++ element/jumbotronElement.js
@@ -6,7 +6,7 @@ class JumbotronElement extends HTMLElement {
     render() {
         this.innerHTML = 
         `
-        <img src="../image/landing1k.png" alt="Using Mask" class="mask">
+        <img src="./image/landing1k.png" alt="Using Mask" class="mask">
         <div class="jumbotron-teks">
             <h1 class="mask-title">Data Pantauan Pasien Covid-19 Seluruh Indonesia</h1>
             <p class="mask-paragraf">Website ini dibuat sebagai bentuk kontribusi yang bisa kami berikan sebagai talenta digital di tengah pandemi virus Covid-19</p>
